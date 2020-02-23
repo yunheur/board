54ef6dadae3e1d072c681b20c91c1132642196eb
yunheur <yunheur@gmail.com>
2020-02-24 00:14:53 +0900

    스프링의 다양한 기능 추가 #3 - Logback 설정

--- src/main/java/com/yunheur/board/board/controller/BoardController.java
+++ src/main/java/com/yunheur/board/board/controller/BoardController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import com.yunheur.board.board.dto.BoardDto;
 import com.yunheur.board.board.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class BoardController {
 
