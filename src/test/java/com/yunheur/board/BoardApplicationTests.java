344ffbbe81f0317a97a45b6bb7d2e3c5ee72472e
yunheur <yunheur@gmail.com>
2020-02-23 20:02:53 +0900

    Create Project

---
+++ src/test/java/com/yunheur/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunheur.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
