40cabbcedf40ed822003fe0f75d2193d71979b57
yunheur <yunheur@gmail.com>
2020-02-24 00:49:38 +0900

    스프링의 다양한 기능 추가 #3 - 인터셉터 사용

---
+++ src/main/java/com/yunheur/board/configuration/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.yunheur.board.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.yunheur.board.board.interceptor.LoggerInterceptor;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry){
+		registry.addInterceptor(new LoggerInterceptor());
+	}
+}
